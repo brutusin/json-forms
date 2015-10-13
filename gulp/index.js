ec0c6c97e8650b17e77ae2bbc0c3dea73e1d7ec4
Ignacio del Valle Alles <idelvall@brutusin.org>
2015-10-13 16:40:54 +0200

    cleaning

--- gulp/index.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var tasks = fs.readdirSync('./gulp/tasks');
-var gulp = require('gulp');
-
-tasks.forEach(function (task) {
-    require('./tasks/' + task);
-});
--- gulp/tasks/default.js
+++
@@ -1,4 +0,0 @@
-var gulp = require('gulp');
-gulp.task('default', [
-    'minify'
-]);
